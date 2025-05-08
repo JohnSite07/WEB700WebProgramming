961850d899d2e0dd29330aee6b335462d96d4881
Jean-Luc <jeen.mb@gmail.com>
2025-05-08 13:53:32 -0400

    Initial commit

---
+++ Version01_js/hello.js
@@ -0,0 +1,19 @@
+console.log("Hello World")
+/*
+Multi line comments
+*/
+
+a = 100
+console.log(a, typeof a)
+a = "Web700"
+console.log(a, typeof a)
+a = true
+console.log(a, typeof a)
+a = null
+console.log(a, typeof a)
+
+let b = 200
+const c = 30
+
+// b = 300 // valid
+// c = 300 // invalid
