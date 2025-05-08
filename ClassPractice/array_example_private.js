b22b25deb3d7a916e4fe16d34b874a16bfc5bf5f
Jean-Luc <jeen.mb@gmail.com>
2025-05-08 14:55:12 -0400

    week 1 examples and practices

---
+++ ClassPractice/array_example_private.js
@@ -0,0 +1,4 @@
+let b = Array(5)
+console.log(b)
+console.log(typeof b)
+console.log(b.length)
---
+++ ClassPractice/func_examples_private.js
@@ -0,0 +1,17 @@
+function greet(name) {
+    const message = `Welcone, ${name}`
+    return message
+}
+
+console.log(greet("Jean Luc"))
+
+ const sayHello2 = () => 
+    console.log("Hello people")
+    console.log("Hello students")
+
+ sayHello2
+
+ const greet = name => {
+    const message = `Welcome, ${name}`
+    return message
+ }
