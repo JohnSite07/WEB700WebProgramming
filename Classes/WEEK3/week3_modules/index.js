e8ca5c92f78fedfce53d3695fa93ca473609c72d
Jean-Luc <jeen.mb@gmail.com>
2025-05-26 12:32:13 -0400

    week3_modules finalized

--- Classes/WEEK3/week3_modules/index.js
+++ Classes/WEEK3/week3_modules/index.js
@@ -3,6 +3,8 @@ const fs = require("fs")
 const http = require("http")
 const os = require("os")
 
+let msg = require("./modules/message.js")
+
 fs.readFile("data.txt", (err, dataRead) => {
     if(err) {
         console.log(err)
@@ -10,4 +12,17 @@ fs.readFile("data.txt", (err, dataRead) => {
         console.log(dataRead.toString())
     }
 })
+
+fs.readFile("user.json", (err, dataRead) => {
+    if(err) {
+        console.log(err)
+    }else{
+        const user = JSON.parse(dataRead.toString())
+        console.log(user)
+        console.log(user.name)
+    }
+})
+
 console.log("Week 03 - END")
+console.log(msg.sayHello)
+msg.sayHello()
---
+++ Classes/WEEK3/week3_modules/modules/message.js
@@ -0,0 +1,7 @@
+const hello = "Welcome to NodeJS module"
+
+function sayHello() {
+    console.log(hello)
+}
+
+module.exports = { hello, sayHello }
